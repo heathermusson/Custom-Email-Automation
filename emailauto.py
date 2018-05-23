fb0f7fb712af3b8308d6d2eb0461710269ca60a6
Heather <hmusson@cogeco.ca>
2018-05-23 17:11:50 -0400

    Added references.txt and made small updates to emailauto.py

--- emailauto.py
+++ emailauto.py
@@ -1,20 +1,18 @@
-#Created by Heather Musson on May 14, 2018 (Last updated May 15, 2018)
-#References:
-    #https://medium.freecodecamp.org/send-emails-using-code-4fcea9df63f
+#Created by Heather Musson on May 14, 2018 (Last updated May 23, 2018)
 
 import smtplib
 from string import Template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-MY_EMAIL = 'email@email.com'
+MY_EMAIL = 'email'
 MY_PASSWORD = 'password'
 
 #get contacts
 def get_contacts(filename):
     names = []
     emails = []
-    with open(filename, mode='r', encoding = 'utf-8')
+    with open(filename, mode='r', encoding = 'utf-8') as contacts_file:
         for a_contact in contacts_file:
             names.append(a_contact.split()[0])
             emails.append(a_contact.split()[1])
@@ -22,15 +20,18 @@ def get_contacts(filename):
 
 #import message
 def read_template(filename):
-    with open(filename, 'r', encodings = 'utf-8') as template_file:
+    with open(filename, 'r', encoding = 'utf-8') as template_file:
         template_file_content = template_file.read()
     return Template(template_file_content)
 
 def main():
     #set up smtp
-    s = smtplib.SMTP(host='your_host_address_here', port = 'port')
-    s.starttls()
-    s.login(MY_EMAIL, MY_PASSWORD)
+    try:
+        s = smtplib.SMTP_SSL(host='smtp.gmail.com', port = '465')
+        s.helo()
+        s.login(MY_EMAIL, MY_PASSWORD)
+    except:
+        print ("Error in setting up email connection")
 
     #import contacts
     names, emails = get_contacts('mycontacts.txt')
@@ -52,10 +53,11 @@ def main():
         msg['To'] = email
         msg['Subject'] = "This is a test"
         msg.attach(MIMEText(message, 'plain')) #adds message body
-        s.send_message(msg)
+        s.sendmail(MY_EMAIL, email, msg)
         del msg
+        print("Message Sent")
 
-    s.quit()
-    
+    s.close()
+
 if __name__ == '__main__':
     main()
