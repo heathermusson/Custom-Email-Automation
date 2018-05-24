7b4bb9f1aa5b7743bfc8c4aadf8a29e65ab3769a
Heather <havmusson@uwaterloo.ca>
2018-05-23 22:03:52 -0400

    Fixed email connection issues, added image and plain-text functionalities

--- emailauto.py
+++ emailauto.py
@@ -4,9 +4,7 @@ import smtplib
 from string import Template
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
-MY_EMAIL = 'email'
-MY_PASSWORD = 'password'
+from email.mime.image import MIMEImage
 
 #get contacts
 def get_contacts(filename):
@@ -28,8 +26,8 @@ def main():
     #set up smtp
     try:
         s = smtplib.SMTP_SSL(host='smtp.gmail.com', port = '465')
-        s.helo()
-        s.login(MY_EMAIL, MY_PASSWORD)
+        s.ehlo()
+        s.login('email', 'password')
     except:
         print ("Error in setting up email connection")
 
@@ -37,25 +35,49 @@ def main():
     names, emails = get_contacts('mycontacts.txt')
 
     #import message template
-    message_template = read_template('message.txt')
+    message_template_html = read_template('messagehtml.txt')
+    message_template_plain = read_template('messageplain.txt')
 
     #send email for each contact
     for name, email in zip(names, emails):
         msg = MIMEMultipart() #creates the message
 
         #put customized name in message
-        message = message_template.substitute(PERSON_NAME = name.title())
+        messagehtml = message_template_html.substitute(PERSON_NAME = name.title())
+        messageplain = message_template_plain.substitute(PERSON_NAME = name.title())
 
         #prints message body
-        print(message)
+        print(messageplain)
 
-        msg['From'] = MY_EMAIL
+        msg['From'] = 'email'
         msg['To'] = email
         msg['Subject'] = "This is a test"
-        msg.attach(MIMEText(message, 'plain')) #adds message body
-        s.sendmail(MY_EMAIL, email, msg)
+
+        #Encapsulate the plain and HTML versions of the message body in an 'alternative' part
+        #Message agents can decide which they want to display
+
+        #Plain-text option
+        msgAlternative = MIMEMultipart('alternative')
+        msg.attach(msgAlternative)
+        msgText = MIMEText(messageplain, 'plain')
+        msgAlternative.attach(msgText)
+
+        #HTML option
+        msgText = MIMEText(messagehtml, 'html')
+        msgAlternative.attach(msgText)
+
+        #Assumes the image is in the current directory
+        fp = open('image.jpg', 'rb')
+        msgImage = MIMEImage(fp.read())
+        fp.close()
+
+        #Define the image's ID as referenced above
+        msgImage.add_header('Content-ID', '<image1>')
+        msg.attach(msgImage)
+
+        s.send_message(msg)
         del msg
-        print("Message Sent")
+        print("Message Sent to " + email)
 
     s.close()
 
